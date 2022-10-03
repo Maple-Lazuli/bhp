71d4e542d27a7a3d3e4013c49b45631f259dc8de
Maple-Lazuli <the.nostra.tymus@gmail.com>
2022-10-03 00:04:33 -0400

    created tcp server and updated the tcp_client to point away from google

--- src/tcp_client.py
+++ src/tcp_client.py
@@ -2,15 +2,15 @@ import socket
 
 if __name__ == "__main__":
 
-    target_host = "www.google.com"
+    target_host = "0.0.0.0"
 
-    target_port = 80
+    target_port = 9998
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     client.connect((target_host, target_port))
 
-    client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+    client.send(b"This is a test of the emergency broadcast system!")
 
     response = client.recv(4096)
 
